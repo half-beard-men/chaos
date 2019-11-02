26409a14a37baded8054bbd51c0e35e84fc95922
Eduardo <eduardo.ruiz@landbot.io>
2019-11-03 00:11:36 +0100

    Added Ejercicio 2

---
+++ Ejercicio2.md
@@ -0,0 +1,2 @@
+# Build
+Ejecuta `npm run build` para compilar todo el proyecto en uno o varios archivos javascript finales, estos son los archivos de distribución.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "./style.css";
-
 var selectors = document.getElementsByClassName("selector");
 for (var i = 0; i < selectors.length; i++) {
   selectors[i].addEventListener("change", function(e) {
